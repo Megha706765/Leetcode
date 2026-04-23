0057afead222c68e9d4c76043742b63cc3e65c5f
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-04-23 14:22:12 +0530

    Time: 10 ms (99.68%), Space: 98 MB (6.49%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set  = new HashSet<>();
+        for(int ele : nums){
+            if(!set.add(ele)) return true;
+        }
+        return false;
+    }
+}
