e44c42e9b6b8e7194c08b5a9d900521bac9dbf77
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-04-29 10:39:29 +0530

    Time: 0 ms (100%), Space: 41.8 MB (89.28%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int copy = x;
+        int sum = 0;
+        while(x > 0){
+            int digit = x % 10;
+            sum = sum + digit;
+            x = x / 10;
+        }
+        if(copy % sum == 0){
+            return sum;
+        }else{
+            return -1;
+        }
+    }
+}
