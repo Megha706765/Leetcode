404455c7b44ddb24c1022dbf48bde0c293517462
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-05-08 20:41:31 +0530

    Time: 1 ms (31.38%), Space: 42.5 MB (46.01%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,17 @@
+// har bar ise 2 se divide karo aur remender kitna ayaga
+// 
+
+
+class Solution {
+    public int hammingWeight(int n) {
+        int m = n;
+        int count = 0;
+        while(m>0){
+             if(m%2 != 0){
+            count++;
+        }
+        m = m/2;
+        }
+        return count;
+    }
+}
