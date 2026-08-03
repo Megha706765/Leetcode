1fd7c3200e8c818c5609d5230a03b677d56f81bb
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-08-04 00:16:34 +0530

    Time: 1 ms (24.35%), Space: 43.6 MB (25.67%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,14 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
-       int n = nums.length;
-    for (int i = 1; i < n; i++) {
-        int key = nums[i]; // Element to insert into the sorted section
-        int j = i - 1;
-        while (j >= 0 && nums[j] > key) { // Shift larger elements right
-            nums[j + 1] = nums[j];
-            j--;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            int ans = nums[i];
+            int j = i-1;
+
+            while(j >= 0 && nums[j] > ans){
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j+1] = ans;
         }
-        nums[j + 1] = key; // Place the element in the correct position
-    }
     }
 }
