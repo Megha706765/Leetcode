411eb4bec9ef1a3e7d913c8b1d6d30a5dfb671a1
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-08-21 20:02:54 +0530

    Time: 1 ms (96.97%), Space: 47.3 MB (60.3%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -0,0 +1,20 @@
+// using new function skip
+class Solution {
+    ListNode dummy = new ListNode(0); //head is dummy
+    ListNode res = dummy; //traversal in LL
+    public void createLL(int x){ //jo val ke equal nhi hai
+        res.next = new ListNode(x);
+        res = res.next;
+    }
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = head;
+        while(temp != null){
+            if(temp.val != val){
+                createLL(temp.val);
+            }
+            temp = temp.next;
+        }
+        //return the linked list
+        return dummy.next;
+    }
+}
