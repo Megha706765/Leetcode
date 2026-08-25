71da69c9681c827cc31d9500e23516046b6253e1
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-08-25 12:05:01 +0530

    Time: 1 ms (99.92%), Space: 42.8 MB (74.1%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(numOnes >= k)return k;
+        k = k-numOnes;
+        if(numZeros >= k)return numOnes;
+        k = k-numZeros;
+        return numOnes - k;
+    }
+}
