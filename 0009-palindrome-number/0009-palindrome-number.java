6b3bcaf875f4b99d301cb952a23c6c536b070ac1
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-08-31 10:28:11 +0530

    Time: 7 ms (10.47%), Space: 45.2 MB (99.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0)return false;
-        int pal = 0;
+        int ans = 0;
         for(int i=x; i!=0; i=i/10){
-            int div = i%10;
-            pal = pal * 10 + div;
+            int rem = i%10;
+            ans = ans * 10 + rem;
         }
-        if(pal == x)return true;
-        else return false;
+        if(ans == x)return true;
+        return false;
     }
 }
