4c31ded37d38e76c24ff87d8bd94fa3e7b63088b
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-08-31 10:40:05 +0530

    Time: 1 ms (98.1%), Space: 42.5 MB (80.39%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -2,10 +2,9 @@ class Solution {
     public int addDigits(int num) {
         while(num > 9){
             int ans = 0;
-            while(num != 0){
-                int rem = num % 10;
+            for(int i=num; i!=0; i=i/10){
+                int rem = i%10;
                 ans = ans + rem;
-                num = num/10;
             }
             num = ans;
         }
