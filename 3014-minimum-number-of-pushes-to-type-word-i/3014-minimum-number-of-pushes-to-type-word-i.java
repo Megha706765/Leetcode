ad444c03d814da9994c48f56b1ae4a9a793dc841
Megha Thakur Lodhi <meghathakur2003123@gmail.com>
2026-09-04 21:55:43 +0530

    Time: 0 ms (100%), Space: 43 MB (73.62%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -6,9 +6,9 @@ class Solution {
             if(i<8){
                 sum = sum + 1;
             }else if(i<16){
-                sum = sum +2;
+                sum = sum + 2;
             }else if(i<24){
-                sum =sum + 3;
+                sum = sum + 3;
             }else{
                 sum = sum + 4;
             }
